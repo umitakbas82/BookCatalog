d169a1c1853bfc00a005249faf2dec406d90c43a
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-28 11:28:07 +0300

    Dbcontext added

---
+++ BookCatalog.Infrastructure/Context/BookCatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookCatalogDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalog.Infrastructure.Context
+{
+    public class BookCatalogDbContext:DbContext
+    {
+        public BookCatalogDbContext(DbContextOptions<BookCatalogDbContext>options):base(options) 
+        {
+            public DbSet<Book> Books { get; set; }
+    }
+    }
+}
